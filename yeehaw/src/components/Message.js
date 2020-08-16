2baaa3ba69b0165ea4eb8d81c964e46d6603daaa
sophie <sophiecliu@gmail.com>
2020-08-15 18:45:38 -0700

    Finish chat (almost)

--- yeehaw/src/components/Message.js
+++ yeehaw/src/components/Message.js
@@ -5,14 +5,15 @@ function Message({ message, date, displayName, profilePic }) {
 	return (
 		<div className="message">
 			<img src={profilePic} alt="" />
-			<div className="message-info">
-				<h4>
+			<div className="message-content">
+				<h4 className="message-info">
 					{displayName}{" "}
 					<span className="message-timestamp">
 						{new Date(date?.toDate()).toUTCString()}
 					</span>
 				</h4>
 				<p>{message}</p>
+				<hr className="bar-hr"/>
 			</div>
 		</div>
 	);
